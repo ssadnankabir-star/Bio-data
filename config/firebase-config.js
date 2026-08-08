7d51d514dbd273799ec3b96076ace8cdc5e789cd
Sadnan <ssadnankabir@gmail.com>
2026-08-08 16:06:15 +0600

    Add files via upload

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,1 +1,18 @@
-
+/*
+  Firebase Web App config.
+  1) Create a Firebase project.
+  2) Enable Authentication > Email/Password.
+  3) Create the admin user with the email shown below.
+  4) Create Firestore.
+  5) Register a Web App and paste its public config here.
+  IMPORTANT: The Firebase Web config is not a password. Never put your admin password here.
+*/
+window.BIODATA_FIREBASE_CONFIG = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+window.BIODATA_ADMIN_EMAIL = "ssadnankabir@gmail.com";
