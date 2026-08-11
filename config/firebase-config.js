59e4dd70619264161dca15dd251b5320e0042f7e
Sadnan <ssadnankabir@gmail.com>
2026-08-11 21:20:02 +0600

    Update Firebase configuration to use global variable
    Replaced Firebase configuration object with global variable.

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,12 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+window.PROFILE_FIREBASE_CONFIG = {
   apiKey: "AIzaSyC5broplCmaQNHGB-PT3fsqEsH_Mc2Stvg",
   authDomain: "sadnan-personal-profile.firebaseapp.com",
   projectId: "sadnan-personal-profile",
@@ -15,7 +7,3 @@ const firebaseConfig = {
   appId: "1:363364801383:web:93fcb97844b8774efcadc2",
   measurementId: "G-GBTFDFG5V8"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
